513140ca309a7796232a13f9ad3dd56446e75609
韩世豪 <hsh@wdtianxia.com>
2018-04-26 22:34:27 +0800

    初始化 1:简单搭建maven加载servlet组件

---
+++ src/main/java/com/herman/chapter1/HelloServlet.java
@@ -0,0 +1,27 @@
+package com.herman.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author hsh
+ * @create 2018-04-26 21:55
+ **/
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {//继承servlet
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
